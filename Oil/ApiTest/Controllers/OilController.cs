bf3531d77a8681ef83e137d1df419a3de7cadf55
MOMANI1 <mmn1078@gmail.com>
2017-04-23 14:51:04 +0300

    init

---
+++ Oil/ApiTest/Controllers/OilController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ApiTest.Controllers
+{
+    public class OilController : ApiController
+    {
+        [HttpGet]
+        public string SayHi()
+        {
+            return "hi";
+        }
+    }
+}
